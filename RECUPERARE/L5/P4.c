9d1e7edcd889c109c5773de2333e2404061b9429
Goder Alexandru <62264515+alexgoder@users.noreply.github.com>
2020-04-24 02:09:12 +0300

    Add files via upload

---
+++ RECUPERARE/L5/P4.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+	int val;
+}stiva;
+
+int isful(int* nr)
+{
+	if (*nr > 6)
+		return 1;
+	return 0;
+}
+
+int isempty(int* nr)
+{
+	if (*nr < 0)
+		return 1;
+	return 0;
+}
+
+void push(stiva* st, int* nr)
+{
+	if (isful(nr) == 1)
+		printf("Nu s-a mai putut introduce un numar deoarece stiva este plina");
+	else
+	{
+		(*nr)++;
+		printf("Introduceti un numar in stiva: ");
+		scanf("%d", &(st + *nr)->val);
+	}
+
+}
+
+void pop(stiva* st, int* nr)
+{
+	if (isempty(nr) == 1)
+		printf("Stiva este goala");
+	else
+	{
+		printf(" %d", (st + *nr)->val);
+		(*nr)--;
+	}
+
+
+}
+
+void peek(stiva* st, int* nr)
+{
+	if (isempty(nr) == 1)
+		printf("Stiva este goala");
+	else
+		printf("%d", (st + *nr)->val);
+}
+
+int main()
+{
+	int opt, n = -1;
+	stiva st[7];
+	do {
+		printf("\n1.Push-introduceti un numar in stiva\n");
+		printf("2.Pop-scoateti ultimul numar din stiva\n");
+		printf("3.Peek-scoateti ultimul numar din stiva\n");
+		printf("Introduceti opriunea dvs:");
+		scanf("%d", &opt);
+		switch (opt)
+		{
+		case 1: push(&st, &n);
+			break;
+		case 2: pop(&st, &n);
+			break;
+		case 3: peek(&st, &n);
+			break;
+		default: exit(0);
+			break;
+		}
+	} while (1);
+	return 0;
+}
