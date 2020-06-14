1e66e488b3f04e03e1cb2ea394b1cc20bc45aa57
Goder Alexandru <62264515+alexgoder@users.noreply.github.com>
2020-06-14 18:54:23 +0300

    Update P7.c

--- RECUPERARE/L5/P7.c
+++ RECUPERARE/L5/P7.c
@@ -1,3 +1,16 @@
+/*Sa se scrie un program care verifica daca un numar citit de
+la tastatura pâna în valoarea de 65535 este palindrom atât în
+baza 2 cât si în baza 10. Verificarea ca numarul ar fi
+palindrom sa se implementeze cu ajutorul unui stack.
+A) Sa se implementeze citirea conditionata pâna la 65535.
+B) Sa se realizeze transformarea din zecimal în binar.
+C) Adaugarea fiecarui bit într-un stack
+D) Adaugarea fiecarei cifre într-un alt stack.
+E) Verificarea ca cele doua variante sunt palindrom sau nu.
+585 = 1001 0 0 1001 (binary) – PALINDROM
+Palindrom – O secventa de caractere care se citeste exact la
+fel indiferent de directia de citire (stânga dreapta sau dreapta
+– stânga)*/
 #include<stdio.h>
 int st[16], st1[5], v[16], v1[5], i, n, j, ok = 1, top = -1, k, top1 = -1, a, ogl;
 
